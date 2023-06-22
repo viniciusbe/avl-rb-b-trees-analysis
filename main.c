4def221f24c463173e1c59e6581d9ac0102893eb
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-06-22 14:06:36 -0300

    Adiciona contagem no arquivo

--- main.c
+++ main.c
@@ -4,40 +4,60 @@
 #include "avl/avl.h"
 #include "utils/utils.h"
 
-#define REPETICOES 10 
+#define REPETICOES 10
 #define TAM_MIN 1 
-#define TAM_MAX 10000
+#define TAM_MAX 1000
 
-void main() {
-    extern long int contadorAVL;
+int main() {
+    extern unsigned long int contadorAVL;
 
-    for (size_t i = TAM_MIN; i < TAM_MAX; i++)
+    printf("Inicio\n");
+
+    FILE *arquivoAdicao;
+    FILE *arquivoRemocao;
+    arquivoAdicao = fopen("graph/adicao.csv", "w+");
+    arquivoRemocao = fopen("graph/remocao.csv", "w+");
+
+    if(arquivoAdicao == NULL || arquivoRemocao == NULL) {
+        printf("Erro ao abrir arquivo(s)");
+        return 0;
+    }
+
+    for (size_t tamChaves = TAM_MIN; tamChaves <= TAM_MAX; tamChaves++)
     {
-        //  Adição
-        int mediaAVL = 0;
+        contadorAVL = 0;
 
         for (size_t j = 0; j < REPETICOES; j++)
         {
-            int *vetor = geraVetor(i);
+            int *chaves = geraVetor(tamChaves);
 
             ArvoreAVL* arvoreAVL = criarArvoreAVL();
 
-            for (size_t k = 0; k < i; k++)
+            for (size_t k = 0; k < tamChaves; k++)
             {
-                /* code */
+                adicionarValorAVL(arvoreAVL, chaves[k]);
             }
-            
 
-            mediaAVL += contadorAVL;
-
+            free(arvoreAVL);
+            free(chaves);
         }
 
-        mediaAVL /= REPETICOES;
-        
-        printf("%d", mediaAVL);
+        // Adição
+
+        fprintf(arquivoAdicao, "%d, %lu\n", tamChaves, contadorAVL / REPETICOES);
 
         //  Remoção
+
+        // TODO
+
+        fflush(arquivoAdicao);
     }
     
-    
+    fclose(arquivoAdicao);
+    fclose(arquivoRemocao);
+
+    printf("Fim\n");
+
+    system("pause");
+    return 1;
 }
