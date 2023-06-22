da4f13659e68089e25fd36fcf5fa78b6b7adc843
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-06-22 14:07:03 -0300

    Plota gráfico

---
+++ graph/gerarGrafico.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Set the figure size
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+plt.rcParams["figure.dpi"] = 600
+
+plt.rcParams["lines.linewidth"] = 1
+
+# Make a list of columns
+headers = ['AVL']
+
+# Read a CSV file
+df = pd.read_csv("graph/adicao.csv", names=headers)
+
+# Plot the lines
+df.plot()
+
+plt.savefig('graph/adicao.png')
